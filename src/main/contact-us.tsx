0850281459f02b11feddfe8c4bd82bb801e2983a
myusername <himashawije5678@gmail.com>
2023-12-28 02:57:52 +0530

    contact-us page styles adding completed

--- src/main/contact-us.tsx
+++ src/main/contact-us.tsx
@@ -1,46 +1,52 @@
 import bannerBackground from '../assets/banner-background.jpg';
 import contactImage from '../assets/quote-frame.png';
+import { IoIosSend } from "react-icons/io";
 
 export const ContactUs = () => {
     const containerInnerStyle = {
         backgroundImage: `url(${bannerBackground})`,
         backgroundSize: 'cover',
         marginBottom: '50px',
-        height: '500px'
+        height: '550px'
     };
     return (
         <div className="container-inner d-flex" style={{ ...containerInnerStyle }}>
             <div className="card mb-3" style={{ maxWidth: "100%", border: '0px', backgroundColor: 'transparent' }}>
                 <div className="row g-0">
                     <div className="col-md-6">
-                        <img src={contactImage} className="img-fluid rounded-start" alt="..." />
+                        <img src={contactImage} className="img-fluid rounded-start" alt="contact-us" />
                     </div>
                     <div className="col-md-6" style={{ marginTop: '50px' }}>
                         <div className="card-body" >
-                            <h5 className="card-title fs-2">Leave Your Feedback Here</h5>
+                            <h5 className="card-title fs-1" style={{ marginBottom: '20px' }}>Share Your Feedback</h5>
+                            <p className="card-text" style={{ marginTop: '10px' }}>
+                                <small className="text-muted">Please use this form both for suggesting improvements to processes and for leave your feedbacks.</small>
+                            </p>
                             <p className="card-text">
                                 <form className="was-validated" >
-                                    <div className="col-md-8">
-                                        <label className="form-label">Your Name</label>
-                                        <input type="text" className="form-control" id="validationCustom01" required />
+                                    <div className="col-md-8" style={{ marginTop: '10px' }}>
+                                        <label className="form-label">Your Name *</label>
+                                        <input type="text" className="form-control" id="validationCustom01" placeholder='Your name is required' required />
                                     </div>
-                                    <div className="col-md-8">
-                                        <label className="form-label">Subject</label>
-                                        <input type="text" className="form-control" id="validationCustom03" required />
+                                    <div className="col-md-8" style={{ marginTop: '10px' }}>
+                                        <label className="form-label">Subject *</label>
+                                        <input type="text" className="form-control" id="validationCustom03" placeholder='Your subject is required' required />
                                     </div>
-                                    <div className="col-md-8">
-                                        <label className="form-label">Your Message</label>
-                                        <textarea className="form-control is-invalid" id="validationTextarea" placeholder="Required example textarea" required></textarea>
+                                    <div className="col-md-8" style={{ marginTop: '10px' }}>
+                                        <label className="form-label">Your Message *</label>
+                                        <textarea className="form-control is-invalid" id="validationTextarea" placeholder="Your message is required" required></textarea>
 
                                     </div>
 
 
-                                    <div className="col-12" style={{ marginTop: '15px' }}>
-                                        <button type="button" className="btn btn-outline-success">Send</button>
+
+                                    <div className="d-grid col-md-8" style={{ marginTop: '20px' }}>
+                                        <button type="button" className="btn btn-light btn-outline-dark"><IoIosSend /> Send</button>
+
                                     </div>
+
                                 </form>
                             </p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                         </div>
                     </div>
                 </div>
